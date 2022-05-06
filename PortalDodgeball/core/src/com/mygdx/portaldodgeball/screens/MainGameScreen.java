fd34aa0a7bbfbc86386df83589d808055c4124a0
Atilla Alp Yavuz <alp.yavuz@ug.bilkent.edu.tr>
2022-05-07 02:19:25 +0300

    Auto stash before merge of "main" and "origin/main"

--- PortalDodgeball/core/src/com/mygdx/portaldodgeball/screens/MainGameScreen.java
+++ PortalDodgeball/core/src/com/mygdx/portaldodgeball/screens/MainGameScreen.java
@@ -6,8 +6,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.portaldodgeball.Entities.Ball;
 import com.mygdx.portaldodgeball.Entities.Player;
-import com.mygdx.portaldodgeball.Entities.map.MapRender;
 import com.mygdx.portaldodgeball.PortalDodgeball;
 
 public class MainGameScreen implements Screen {
@@ -23,8 +23,6 @@ public class MainGameScreen implements Screen {
         this.game = game;
     }
 
-
-
     @Override
     public void show() {
     }
@@ -35,15 +33,6 @@ public class MainGameScreen implements Screen {
         ScreenUtils.clear(1, 0, 0, 1);
         Gdx.gl.glClearColor(0/255f, 98/255f, 228/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        drawMap();
-//        MapRender aMap = new MapRender(1, game);
-/*
-        for(int i = Wx; i<Wheight + Wx ; i =+ WunitSize){
-            for(int j = Wy; j<Wwidth + Wy; j =+ WunitSize){
-                game.batch.draw(WwallUnit,i,j);
-            }
-        }
- */
         switch (game.players.length){
             case 2:
                 game.players[0].move();
@@ -53,6 +42,7 @@ public class MainGameScreen implements Screen {
                 game.players[0].move();
                 game.players[1].move();
                 game.players[2].move();
+
                 break;
             case 4:
                 game.players[0].move();
@@ -69,14 +59,21 @@ public class MainGameScreen implements Screen {
                 game.players[5].move();
                 break;
         }
+
         for(int i = 0; i < game.players.length; i++){
             game.batch.draw(game.players[i].texture, game.players[i].x, game.players[i].y);
+            for (Ball ball: game.players[i].balls) {
+                ball.draw(game.batch);
+            }
         }
+
         if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)){
             game.setScreen(new MainMenu(game));
             Player.id = 0;
             game.setPlayers(new Player[]{});
         }
+
+
         game.batch.end();
     }
     public void drawMap(){
@@ -85,23 +82,13 @@ public class MainGameScreen implements Screen {
         int WunitSize = 3;
         Texture wallUnit = new Texture("Gameplay sprites/wall unit piece.png");
 
-        for(int i = 0; i < map.length; i++){
-            int Wx = map[i][0];
-            int Wy= map[i][1];
-            int Wheight = map[i][2];
-            int Wwidth = map[i][3];
-            int a = Wy;
-            int b = Wx;
-            while(b < Wx+ Wwidth){
-                while(a< Wy + Wheight){
-                    game.batch.draw(wallUnit,b,a);
-                    a += WunitSize;
-                }
-                b += WunitSize;
-                a =Wy;
+        for(int i = 0; i < game.players.length; i++){
+            for (Ball ball: game.players[i].balls) {
+                ball.update(Gdx.graphics.getDeltaTime());
             }
-            int r = 6;
         }
+
+        game.inputManager.update();
     }
 
     @Override
