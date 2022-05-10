25adf83f7cce3839972dc4bfd0c132f959f6c0eb
Tolga Artun Koçak <artun.kocak@bilkent.edu.tr>
2022-05-10 17:53:29 +0300

    Implementing a fix for when portals get stuck in corners

--- PortalDodgeball/core/src/com/mygdx/portaldodgeball/Entities/StillPortal.java
+++ PortalDodgeball/core/src/com/mygdx/portaldodgeball/Entities/StillPortal.java
@@ -30,12 +30,13 @@ public class StillPortal {
         this.y = y;
         findWallHit();
         calcRotation();
+        boolean check = canBePlaced();
 
         if(direction == 2 || direction == 4){
-            this.hitbox = new Hitbox(this.x-11, this.y-20,width,height);
+            this.hitbox = new Hitbox(this.x-20, this.y,width,height);
         }
         else if(direction == 1 || direction == 3){
-            this.hitbox = new Hitbox(this.x-20, this.y-11,width,height);
+            this.hitbox = new Hitbox(this.x, this.y-20,width,height);
         }
     }
     public void Alternator() {
@@ -61,13 +62,63 @@ public class StillPortal {
     public void draw(SpriteBatch spriteBatch) {
         spriteBatch.draw(texture,hitbox.x,hitbox.y,width,height);
     }
-    //    public boolean canBePlaced(){
-    //}
+
+    public boolean canBePlaced(){
+        Hitbox hitboxCheck1;
+        Hitbox hitboxCheck2;
+        boolean canBePlaced = true;
+        Wall wrongWall = new Wall(1,1,1,1);
+        if(direction == 2 || direction == 4){
+            hitboxCheck1 = new Hitbox(this.x-11, this.y-20,width/2,height);
+            hitboxCheck2 = new Hitbox(this.x-11, this.y,width/2,height);
+        }
+        else{
+            hitboxCheck1 = new Hitbox(this.x-20, this.y-11,width,height/2);
+            hitboxCheck2 = new Hitbox(this.x, this.y-11,width,height/2);
+        }
+        for(int i = 0; i < MapRender.walls.length; i++){
+            if(MapRender.walls[i] != this.wallHit){
+                if(hitboxCheck1.collidesWidth(MapRender.walls[i].wallHitbox) || hitboxCheck2.collidesWidth(MapRender.walls[i].wallHitbox)){
+                    canBePlaced = false;
+                    wrongWall = MapRender.walls[i];
+                    break;
+                }
+            }
+        }
+        while (!canBePlaced){
+            if(hitboxCheck1.collidesWidth(wrongWall.wallHitbox)){
+                if(direction == 2 || direction == 4){
+                    hitboxCheck1.x++;
+                    x++;
+                }
+                else {
+                    hitboxCheck1.y++;
+                    y++;
+                }
+            }
+            if(hitboxCheck2.collidesWidth(wrongWall.wallHitbox)){
+                if(direction == 2 || direction == 4){
+                    hitboxCheck2.x--;
+                    x--;
+                }
+                else{
+                    hitboxCheck2.y--;
+                    y--;
+                }
+
+            }
+            else{
+                canBePlaced = true;
+            }
+        }
+        return  canBePlaced;
+
+    }
     public void findWallHit(){
-        Hitbox tester = new Hitbox(x+11,y+11,1,1);
-        Hitbox tester2 = new Hitbox(x+11,y-1,1,1);
-        Hitbox tester3 = new Hitbox(x-1,y+11,1,1);
-        Hitbox tester4 = new Hitbox(x-1,y-1,1,1);
+        Hitbox tester = new Hitbox(x+11,y+11,2,2);
+        Hitbox tester2 = new Hitbox(x+11,y-1,2,2);
+        Hitbox tester3 = new Hitbox(x-1,y+11,2,2);
+        Hitbox tester4 = new Hitbox(x-1,y-1,2,2);
         for(int i = 0; i< MapRender.walls.length;i++){
             Wall testWall = MapRender.walls[i];
             if(tester.collidesWidth(testWall.wallHitbox)||tester2.collidesWidth(testWall.wallHitbox)||tester3.collidesWidth(testWall.wallHitbox)||tester4.collidesWidth(testWall.wallHitbox)){
@@ -81,12 +132,12 @@ public class StillPortal {
             if(y>wallHit.wallHitbox.y){
                 direction = 2;
                 texture = new Texture("Portals/p1InDown.png");
-                this.y = wallHit.y+29;
+                this.y = wallHit.y+9;
             }
             else{
                 direction = 4;
                 texture = new Texture("Portals/p1InUp.png");
-                this.y = wallHit.y;
+                this.y = wallHit.y-21;
             }
             width = 39;
             height = 21;
@@ -95,12 +146,12 @@ public class StillPortal {
             if(x>(wallHit.wallHitbox.x)){
                 direction = 3;
                 texture = new Texture("Portals/p1InLeft.png");
-                this.x = wallHit.x+29;
+                this.x = wallHit.x+9;
             }
             else{
                 direction = 1;
                 texture = new Texture("Portals/p1InRight.png");
-                this.x = wallHit.x;
+                this.x = wallHit.x-21;
             }
             width = 21;
             height = 39;
